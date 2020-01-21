8ce5b5189f88165aeffdea934bf51d3a41afb8d6
ashen7 <986300260@qq.com>
2020-01-21 22:07:03 +0800

    卷积层类声明

---
+++ include/convolutional_layer.h
@@ -0,0 +1,156 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  convolutional_layer.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2019年12月29日 19时38分17秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  yipeng 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#ifndef CNN_CONVOLUTIONAL_LAYER_H_
+#define CNN_CONVOLUTIONAL_LAYER_H_
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include <memory>
+#include <vector>
+#include <functional>
+
+#include <glog/logging.h>
+
+#include "utility/singleton.hpp"
+
+namespace cnn {
+//类的前置声明
+class Filter;
+
+//全连接层实现类
+class ConvolutionalLayer {
+public:
+    typedef std::vector<double> Matrix1d;
+    typedef std::vector<std::vector<double>> Matrix2d;
+    typedef std::vector<std::vector<std::vector<double>>> Matrix3d;
+    typedef std::vector<std::vector<uint8_t>> ImageMatrix2d;
+    typedef std::vector<std::vector<std::vector<uint8_t>>> ImageMatrix3d;
+    typedef std::function<void(const Matrix3d&, Matrix3d&)> ReLuActivatorCallback;
+
+    ConvolutionalLayer();
+    ~ConvolutionalLayer();
+    ConvolutionalLayer(const ConvolutionalLayer&) = delete;
+    ConvolutionalLayer& operator =(const ConvolutionalLayer&) = delete;
+    ConvolutionalLayer(ConvolutionalLayer&&) = default;
+    ConvolutionalLayer& operator =(ConvolutionalLayer&&) = default;
+    
+    //计算输出特征图的宽高
+    //特征图大小 = (输入大小 - 卷积核大小 + 2*补0填充) / 步长 + 1
+    static int CalculateOutputHeight(int input_height, int filter_height,
+                                     int zero_padding, int stride) {
+        return static_cast<int>((input_height - filter_height + 2 * zero_padding) / stride + 1);
+    }
+
+    static int CalculateOutputWidth(int input_width, int filter_width,
+                                    int zero_padding, int stride) {
+        return static_cast<int>((input_width - filter_width + 2 * zero_padding) / stride + 1);
+    }
+
+    static void set_relu_forward_callback(ReLuActivatorCallback forward_callback) {
+        relu_forward_callback_ = forward_callback;
+    }
+
+    static void set_relu_backward_callback(ReLuActivatorCallback backward_callback) {
+        relu_backward_callback_ = backward_callback;
+    }
+    
+    static ReLuActivatorCallback get_relu_forward_callback() { 
+        return relu_forward_callback_;
+    }
+
+    static ReLuActivatorCallback get_relu_backward_callback() {
+        return relu_backward_callback_;
+    }
+
+    void set_filters(const std::vector<Matrix3d>& filters_weights, 
+                     const std::vector<double> filters_biases) noexcept;
+
+    const Matrix3d& get_output_array() const noexcept {
+        return output_array_;
+    }
+
+    const Matrix3d& get_delta_array() const noexcept {
+        return delta_array_;
+    }
+
+    bool is_image_matrix() const noexcept {
+        if (0 == input_array_.size()
+                || 0 != image_input_array_.size()) {
+            return true;
+        } else if (0 != input_array_.size()
+                || 0 == image_input_array_.size()) {
+            return false;
+        } else {
+            LOG(FATAL) << "programming occur error, \
+                           conv input array or image input array \
+                           not initialize, programm is exiting";
+            return false;
+        }
+    }
+
+public:
+    int Initialize(int input_height, int input_width, int channel_number, 
+                   int filter_height, int filter_width, int filter_number, 
+                   int zero_padding, int stride); 
+    int Forward(const Matrix3d& input_array);
+    int Forward(const ImageMatrix3d& input_array);
+    int Backward(const Matrix3d& sensitivity_array);
+    int Backward(const Matrix3d& input_array, 
+                 const Matrix3d& sensitivity_array);
+    void UpdateWeights(double learning_rate, int batch_size);
+    
+    int CalcBpGradient(const Matrix3d& sensitivity_array);
+    int CalcBpSensitivityMap(const Matrix3d& sensitivity_array);
+    int ExpandSensitivityMap(const Matrix3d& input_sensitivity_array, 
+                             Matrix3d& output_sensitivity_array);
+    void Dump() const noexcept;
+
+    int GradientCheck(const Matrix3d& input_array);
+
+protected:
+    static ReLuActivatorCallback relu_forward_callback_;  //激活函数的前向计算
+    static ReLuActivatorCallback relu_backward_callback_; //激活函数的反向计算 
+
+private:
+    int input_height_;      //输入图像高
+    int input_width_;       //输入图像宽
+    int channel_number_;    //输入图像深度
+    int filter_height_;     //卷积核高
+    int filter_width_;      //卷积核宽
+    int filter_number_;     //卷积核个数
+    int zero_padding_;      //补0填充
+    int stride_;            //卷积时移动的步长
+    int output_height_;     //输出特征图高
+    int output_width_;      //输出特征图宽
+    Matrix3d input_array_;                   //输入数组
+    ImageMatrix3d image_input_array_;        //图像输入数组
+    Matrix3d padded_input_array_;            //补0填充后的输入数组
+    ImageMatrix3d padded_image_input_array_; //补0填充后的图像输入数组
+    Matrix3d output_array_;                  //输出特征图数组
+    Matrix3d delta_array_;                   //通过本层敏感图计算得到 上一层的敏感图(误差项) 这是保存上一层的误差项 
+    std::vector<std::shared_ptr<Filter>> filters_;  //filter对象数组
+};
+
+
+}      //namespace cnn
+
+//单例模式
+typedef typename utility::Singleton<cnn::ConvolutionalLayer> SingletonConvLayer;
+
+#endif //CNN_CONVOLUTIONAL_LAYER_H_
